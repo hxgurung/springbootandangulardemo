d04c8048b8477773eed9db7230b995925f2126f3
haridgrg <65652831+haridgrg@users.noreply.github.com>
2021-08-17 02:38:37 -0500

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Car } from './model/car.model';
+
+import { CarService } from './service/car.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'multiplescreendemo';
+export class AppComponent implements OnInit {
+  title = 'dogs-app';
+  cars:Car[]=[];
+  //Constructor injection
+  constructor(private carService:CarService){
+  }
+
+  ngOnInit(): void {
+      this.cars= this.carService.findCar();
+  }
+
 }
---
+++ src/app/model/car.model.ts
@@ -0,0 +1,6 @@
+export class Car {
+    constructor(public did:number, public photo:string,public make:string, public model:string, public color:string, public price :number, public dob:Date ){
+        
+    }
+
+}
